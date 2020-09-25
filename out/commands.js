282ebd4ebfffe9fc08e63253438c056f09b4c2e8
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2020-09-25 12:33:43 -0500

    Implement documentation search

--- out/commands.js
+++ out/commands.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
+const open = require("open");
 
 function loadInRepl(repls) {
     vscode.window.showInformationMessage('Loaded');
@@ -8,6 +9,15 @@ function loadInRepl(repls) {
 exports.loadInRepl = loadInRepl;
 
 function searchDocumentation() {
-    vscode.window.showInformationMessage('Searched');
+    let editor = vscode.window.activeTextEditor;
+    if (editor) {
+        let trimmed = editor.document.getText(editor.selection).trim();
+        if (trimmed) {
+            open("https://docs.racket-lang.org/search/index.html?q=T%3Adssl2 " + trimmed);
+        }
+    }
+    else {
+        vscode.window.showErrorMessage("A file must be opened before you can do that");
+    }
 }
 exports.searchDocumentation = searchDocumentation;
